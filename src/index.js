b8fd8fb2ab0442bb613b81f16d7db164728b726c
Yourdad404 <arshadaamir09@gmail.com>
2023-08-08 09:39:09 +0530

    setting up my node-template

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express=require('express');
+const app=express();
+const bodyParser=require('body-parser')
+const {PORT}=require('./config/serverConfig');
+const apiRoutes=require('./routes/index')
+const db=require('./models/index')
+const setupandStartServer=()=>{
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}))
+     app.use('/api',apiRoutes)    
+
+    app.listen(PORT,()=>{
+          console.log(`Server started at Port ${PORT}`)
+            if(process.env.DB_SYNC){
+                db.sequelize.sync({alter:true});
+            }
+    })
+}
+setupandStartServer();
